1a7fec76b5e7f3c01fc388aae3828d9505d8e021
aleksmn <al-mn@yandex.ru>
2022-06-29 18:27:33 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
+
+// LEARN MORE:
+// https://www.youtube.com/watch?v=VsUzmlZfYNg
---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// LEARN MORE:
+// https://www.youtube.com/watch?v=VsUzmlZfYNg
